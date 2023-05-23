d8021eff4331c26a094f184290a3cecd7666ea50
r0635847 <dries.decuyper@student.ucll.be>
2023-05-23 11:07:53 +0200

    initial commit

---
+++ Assets/Scripts/ARPlacement.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    public GameObject arObjectToSpawn;
+    public GameObject placementIndicator;
+
+    private GameObject spawnedObject;
+    private Pose placementPose;
+    private ARRaycastManager aRRaycastManager;
+
+    private bool placementPoseIsValid = false;
+
+
+    void Start()
+    {
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            ARPlaceObject();
+        }
+
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (spawnedObject == null && placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+
+    private void ARPlaceObject()
+    {
+        spawnedObject = Instantiate(arObjectToSpawn, placementPose.position, placementPose.rotation * Quaternion.Euler(0, 180, 0));
+    }
+}
---
+++ Assets/Scripts/CarAssemblyManager.cs
@@ -0,0 +1,174 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarAssemblyManager : MonoBehaviour
+{
+    public GameObject[] assemblyObjects;
+    public Transform assemblySpot;
+
+    public float forwardOffset = 1f;
+    public float lowerOffset = 0.5f;
+
+    private TextMeshProUGUI debug;
+    private Canvas ui;
+
+    private GameObject arCamera;
+
+    private int currentObjectIndex = 0;
+    private GameObject currentAssemblyObject;
+    private bool isObjectPlaced = false;
+
+    private void Start()
+    {
+        arCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        debug = GameObject.FindGameObjectWithTag("Debug").GetComponent<TextMeshProUGUI>();
+        ui = GameObject.FindGameObjectWithTag("UI").GetComponent<Canvas>();
+
+        ui.transform.Find("Up").GetComponent<Button>().onClick.AddListener(() => MoveUp());
+        ui.transform.Find("Down").GetComponent<Button>().onClick.AddListener(() => MoveDown());
+        ui.transform.Find("RotateL").GetComponent<Button>().onClick.AddListener(() => RotateLeft());
+        ui.transform.Find("RotateR").GetComponent<Button>().onClick.AddListener(() => RotateRight());
+        ui.transform.Find("Futher").GetComponent<Button>().onClick.AddListener(() => MoveFurther());
+        ui.transform.Find("Closer").GetComponent<Button>().onClick.AddListener(() => MoveCloser());
+        ui.transform.Find("Right").GetComponent<Button>().onClick.AddListener(() => MoveRight());
+        ui.transform.Find("Left").GetComponent<Button>().onClick.AddListener(() => MoveLeft());
+        ui.transform.Find("Confirm").GetComponent<Button>().onClick.AddListener(() => ConfirmPlacement());
+
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began && !isObjectPlaced)
+            {
+                if (currentObjectIndex < assemblyObjects.Length)
+                {
+                    SpawnAssemblyObject();
+                    isObjectPlaced = true;
+                }
+            }
+        }
+    }
+
+    private void SpawnAssemblyObject()
+    {
+        Vector3 spawnPosition = arCamera.transform.position + (arCamera.transform.forward * forwardOffset);
+        spawnPosition.y -= lowerOffset;
+
+        currentAssemblyObject = Instantiate(assemblyObjects[currentObjectIndex], spawnPosition, Quaternion.identity);
+        debug.text = "object instantiate";
+    }
+
+    public void ConfirmPlacement()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "not null";
+            if (IsObjectInCorrectPosition())
+            {
+                debug.text = "correct position";
+                PlaceObjectInAssemblySpot();
+            }
+            else
+            {
+                debug.text = "remove object";
+                Destroy(currentAssemblyObject);
+                isObjectPlaced = false;
+            }
+        }
+    }
+
+    private bool IsObjectInCorrectPosition()
+    {
+        float distanceThreshold = 0.1f;
+        float distance = Vector3.Distance(currentAssemblyObject.transform.position, assemblySpot.position);
+
+        return distance <= distanceThreshold;
+    }
+
+    private void PlaceObjectInAssemblySpot()
+    {
+        debug.text = "placing object";
+        currentAssemblyObject.transform.position = assemblySpot.position;
+        currentAssemblyObject.transform.rotation = assemblySpot.rotation;
+        currentObjectIndex++;
+        isObjectPlaced = false;
+    }
+
+    public void MoveUp()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "up";
+            currentAssemblyObject.transform.Translate(Vector3.up * 0.025f);
+        }
+    }
+
+    public void MoveDown()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "down";
+            currentAssemblyObject.transform.Translate(Vector3.down * 0.025f);
+        }
+    }
+
+    public void MoveLeft()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "left";
+            currentAssemblyObject.transform.Translate(Vector3.left * 0.025f);
+        }
+    }
+
+    public void MoveRight()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "right";
+            currentAssemblyObject.transform.Translate(Vector3.right * 0.025f);
+        }
+    }
+
+    public void MoveFurther()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "further";
+            currentAssemblyObject.transform.Translate(Vector3.forward * 0.025f);
+        }
+    }
+
+    public void MoveCloser()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "closer";
+            currentAssemblyObject.transform.Translate(Vector3.back * 0.025f);
+        }
+    }
+
+    public void RotateLeft()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "left";
+            currentAssemblyObject.transform.Rotate(Vector3.up, -10f);
+        }
+    }
+
+    public void RotateRight()
+    {
+        if (currentAssemblyObject != null)
+        {
+            debug.text = "right";
+            currentAssemblyObject.transform.Rotate(Vector3.up, 10f);
+        }
+    }
+}
